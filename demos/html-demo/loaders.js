49afc9cd1230097b0e65ab54030c661ac0c5e5c1
Dusan Mijatovic (PC2016) <d.mijatovic@dv4all.nl>
2020-02-29 18:50:59 +0100

    html demo extended

---
+++ demos/html-demo/loaders.js
@@ -0,0 +1,50 @@
+/**
+ * LOADERS DEMO script
+ * here we show/hide loaders based on buttons clicked
+ */
+const btns = document.querySelectorAll('.btn-show-loader')
+const loaderSection = document.getElementById('loader-area')
+const standardMsg = 'In this section we inject loader element.'
+/**
+ * Add loader to loader-area section
+ * @param {String} type custom element type (loader type)
+ */
+function addLoader(type){
+  //create loader element
+  let el = document.createElement(type)
+  //add overlay
+  el.setAttribute('overlay', true)
+  //set Loading... text
+  el.innerText = 'Loading...'
+  //listen to destroy
+  el.addEventListener('click', clearLoader)
+  //append to loader section
+  loaderSection.appendChild(el)
+}
+/**
+ * Remove elements from loader-area section
+ */
+function clearLoader(){
+  loaderSection.innerHTML = standardMsg
+}
+/**
+ * Listens for click on button to create loader element.
+ * The data attribute of the button indicates which type
+ * of loader to be shown
+ */
+function listenForBtnClick(){
+  btns.forEach(btn=>{
+    btn.addEventListener('click',function ({target}){
+      // console.log("clicked...", target)
+      const data = target.getAttribute('data-loader-type')
+      if (data) addLoader(data)
+    })
+  })
+}
+
+//show standard message
+loaderSection.innerHTML = standardMsg
+
+//listen for click on buttons
+listenForBtnClick()
+
---
+++ demos/html-demo/styles/layout.js
@@ -0,0 +1,83 @@
+/**
+ * Page layout components. Here we use custom web components to play a bit :-)
+ * The header and the footer of all pages in this demo are the same, except fot active
+ * class on the route. To avoid writing this code for each page we use custom components
+ * defined here.
+ */
+
+
+/**
+ * DEFINE PAGES for the header
+ */
+const routes=[
+  {href:'/index.html', label:'Home', route:'/'},
+  {href:'/icons.html', label:'Icons', route:'/icons'},
+  {href:'/loaders.html', label:'Loaders', route:'/loaders'},
+  {href:'/components.html', label:'Components', route:'/components'}
+]
+
+/**
+ * page header for all pages
+ */
+class Dv4PageHeader extends HTMLElement{
+  constructor(){
+    super()
+  }
+  /**
+   * Lifecycle event when component is mounted to DOM
+   */
+  connectedCallback(){
+    console.log(`${this.localName}...mounted to DOM!`)
+    //init with props (attribute values
+    this.render()
+  }
+  getRoutes(){
+    const activeRoute = window.location.pathname
+    let html=''
+    routes.forEach(r=>{
+      html+=`
+        <a href="${r.href}"
+          class="link ${activeRoute === r.route ? 'active' : ''}">
+          ${r.label}
+        </a>
+      `
+    })
+    return html
+  }
+  render(){
+    this.innerHTML=`
+      <h1>Web components DEMO</h1>
+      <nav>
+        ${this.getRoutes()}
+      </nav>
+    `
+  }
+}
+
+/**
+ * page footer for all pages
+ */
+class Dv4PageFooter extends HTMLElement{
+  constructor(){
+    super()
+  }
+  /**
+   * Lifecycle event when component is mounted to DOM
+   */
+  connectedCallback(){
+    console.log(`${this.localName}...mounted to DOM!`)
+    //init with props (attribute values
+    this.render()
+  }
+  /**
+   * Render html
+   */
+  render(){
+    this.innerHTML=`
+      <h2>This is my footer</h2>
+    `
+  }
+}
+
+customElements.define('dv4-page-header',Dv4PageHeader)
+customElements.define('dv4-page-footer',Dv4PageFooter)
